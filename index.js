98243f9656611fa35fe010254c5def7957d7df15
shristi-rks <shristi.n08@gmail.com>
2022-10-27 20:04:16 +0300

    Define JS function bingo

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const string = (text) => {
-    return text;
-} 
+const string = (text) => text;
+
+//Create a function that takes a number as a parameter and returns "Bingo" if the number is greater than 5. 
+const bingo = (num) => {
+    let result = "";
+    num > 5 ? result = "Bingo" : result = "Better luck next time";
+    return result;
+}
 
 string("Shristi");
 console.log(string("Shristi"));
+
+bingo(9);
+bingo(2);
+console.log(bingo(9));
+console.log(bingo(2));
